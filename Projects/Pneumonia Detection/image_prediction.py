5d22056a7da419e18864d1dc46072bece0d97b92
Hariharasudhan G <80266211+sudhxan@users.noreply.github.com>
2021-08-29 17:50:41 +0530

    Add files via upload

---
+++ Projects/Pneumonia Detection/image_prediction.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: harih
+"""
+
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+model = load_model('model_vgg19.h5')
+img = image.load_img('val/PNEUMONIA/person1946_bacteria_4874.jpeg', target_size=(224,224))
+x = image.img_to_array(img)
+x = np.expand_dims(x,axis=0)
+img_data = preprocess_input(x)
+classes = model.predict(img_data)
